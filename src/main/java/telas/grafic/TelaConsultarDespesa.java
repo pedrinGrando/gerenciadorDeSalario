a72394ac2c1eeb70428c9d593aa6b1d1b9daa326
Pedro <Pedro@jc-5655.jucesc.sc.gov.br>
2023-05-16 12:12:07 -0300

    Atualização tela consulta de despesa

--- src/main/java/telas/grafic/TelaConsultarDespesa.java
+++ src/main/java/telas/grafic/TelaConsultarDespesa.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.JTextComponent;
 
 import model.dao.DespesaDAO;
 import model.dao.UserDAO;
@@ -12,6 +13,8 @@ import model.vo.DespesaVO;
 import model.vo.UserVO;
 
 import java.awt.Color;
+import java.awt.Container;
+
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -39,11 +42,8 @@ public class TelaConsultarDespesa extends JFrame {
 	
 	
 	private JPanel contentPane;
-	private JTextField nomeDesp_camp;
-	private JTextField data_camp;
 	private JTextField textField;
 	private JTextField textField_valor;
-	private JTextField campoValor_consultado;
 
 	/**
 	 * Launch the application.
@@ -114,44 +114,32 @@ public class TelaConsultarDespesa extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		nomeDesp_camp = new JTextField();
-		nomeDesp_camp.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 10));
-		nomeDesp_camp.setBackground(new Color(64, 128, 128));
-		nomeDesp_camp.setEditable(false);
-		nomeDesp_camp.setBounds(115, 177, 86, 20);
-		contentPane.add(nomeDesp_camp);
-		nomeDesp_camp.setColumns(10);
-		
-		data_camp = new JTextField();
-		data_camp.setBackground(new Color(64, 128, 128));
-		data_camp.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 10));
-		data_camp.setEditable(false);
-		data_camp.setBounds(243, 198, 105, 20);
-		contentPane.add(data_camp);
-		data_camp.setColumns(10);
-		
 		JLabel lblNewLabel = new JLabel("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 		lblNewLabel.setBounds(0, 141, 613, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Nome da Despesa");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.ITALIC, 10));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 10));
 		lblNewLabel_1.setBounds(115, 164, 100, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Valor");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
 		lblNewLabel_2.setBackground(new Color(192, 192, 192));
 		lblNewLabel_2.setBounds(115, 208, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Pagamento");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.ITALIC, 10));
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 10));
 		lblNewLabel_3.setBounds(244, 183, 85, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("Consultar Despesa");
 		btnNewButton.addActionListener(new ActionListener() {
+			private JLabel lblPag;
+			private JLabel lblValor;
+			private JLabel lblNomeDesp;
+
 			public void actionPerformed(ActionEvent e) {
 				
 				if (userLogado.getIdUsuario() == 0) {
@@ -167,27 +155,28 @@ public class TelaConsultarDespesa extends JFrame {
 					JOptionPane.showMessageDialog(null, "Despesa não encontrada!","GS - Gerenciador de salário", JOptionPane.WARNING_MESSAGE);
 				} else if (despesaVO.getIdDespesa() != 0) {
 					
-				nomeDesp_camp = new JTextField(despesaVO.getDivnome());
-				nomeDesp_camp.setBounds(115, 177, 86, 20);
-				contentPane.add(nomeDesp_camp);
-				nomeDesp_camp.setColumns(10);
+				
 				//DATA FORMATADA
 				DateTimeFormatter fin = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 				LocalDate date = despesaVO.getUltimoPagamento();
 				
 				String valorF = ""+despesaVO.getValor();
 				
-				campoValor_consultado = new JTextField("R$ "+despesaVO.getValor());
-				campoValor_consultado.setBackground(new Color(64, 128, 128));
-				campoValor_consultado.setEditable(false);
-				campoValor_consultado.setBounds(115, 223, 86, 20);
-				contentPane.add(campoValor_consultado);
-				campoValor_consultado.setColumns(10);
+				lblNomeDesp = new JLabel("");
+				lblNomeDesp.setBounds(125, 183, 90, 14);
+				contentPane.add(lblNomeDesp);
+				lblNomeDesp.setText(despesaVO.getDivnome());
 				
-				data_camp = new JTextField(date.format(fin));
-				data_camp.setBounds(243, 198, 86, 20);
-				contentPane.add(data_camp);
-				data_camp.setColumns(10);
+				lblValor = new JLabel("");
+				lblValor.setBounds(125, 240, 108, 14);
+				contentPane.add(lblValor);
+				lblValor.setText("R$ "+despesaVO.getValor());
+				
+				lblPag = new JLabel("");
+				lblPag.setBounds(267, 208, 90, 14);
+				contentPane.add(lblPag);
+				lblPag.setText(""+despesaVO.getUltimoPagamento());
+				
 				}
 			}
 		});
@@ -221,11 +210,8 @@ public class TelaConsultarDespesa extends JFrame {
 		btnNewButton_1.setBounds(0, 305, 33, 23);
 		contentPane.add(btnNewButton_1);
 		
-		campoValor_consultado = new JTextField();
-		campoValor_consultado.setBackground(new Color(64, 128, 128));
-		campoValor_consultado.setEditable(false);
-		campoValor_consultado.setBounds(115, 223, 86, 20);
-		contentPane.add(campoValor_consultado);
-		campoValor_consultado.setColumns(10);
+		
+		
+		
 	}
 }
